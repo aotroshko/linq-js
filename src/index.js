fdc19c25b46bc19812ba8e6504752218fa483e8b
aotroshko <a.otroshko@svitla.com>
2016-08-27 21:59:43 +0300

    Added Where condition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import where from './where';
-// Some comment
-console.log('index.js');
+
+let targetArray = Symbol();
+
+class Enumerable {
+    constructor(arr) {
+        if(Object.prototype.toString.call(arr) === '[object Array]'){
+            this[targetArray] = arr;
+        } else if(typeof arr == 'string' || arr instanceof String){
+            this[targetArray] = arr.split('');
+        } else {
+            throw new Error('Given parameter is not an array.');
+        }
+    }
+
+    where(predicate) {
+        let result = where(this[targetArray], predicate);
+        this[targetArray] = result;
+        return this;
+    }
+}
+
+Array.prototype.asEnumerable = String.prototype.asEnumerable = function() {
+    return new Enumerable(this);
+};
+
+export default Enumerable;
--- src/where.js
+++ src/where.js
@@ -1,1 +1,20 @@
-console.log('where.js');
+export default (targetArray, predicate) => {
+    if(!targetArray) {
+        throw new Error('targetArray is not defined');
+    }
+
+    if(!predicate) {
+        throw new Error('predicate is not defined');
+    }
+
+    let result = [];
+
+    for (let i = 0, len = targetArray.length; i < len; i++) {
+        let item = targetArray[i];
+        if (predicate(item)) {
+            result.push(item);
+        }
+    }
+
+    return result;
+}
