69e69eb5068cfdca9c27aec3430671bcdf4b8f85
aotroshko <otroshko@gmail.com>
2016-08-28 14:39:07 +0300

    Removed JSDoc comments

--- src/enumerable.js
+++ src/enumerable.js
@@ -2,9 +2,6 @@ import where from './where.js';
 
 let source = Symbol();
 
-/**
- * Provides a set of functions for querying objects from arrays.
- */
 class Enumerable {
 	constructor(arr) {
 		if (Object.prototype.toString.call(arr) === '[object Array]') {
@@ -16,19 +13,10 @@ class Enumerable {
 		}
 	}
 
-	/**
-	 * Returns an array that contains the elements from the input sequence.
-	 * @returns {Array} Еhe resulting array of processed data.
-	 */
 	toArray() {
 		return this[source];
 	}
 
-	/**
-	 * Filters a sequence of values based on a predicate.
-	 * @param {(Function|String)} predicate A function to test each element for a condition.
-	 * @returns {Enumerable} Filtered sequence of values that satisfy the condition.
-	 */
 	where(predicate) {
 		this[source] = where(this[source], predicate);
 		return this;
