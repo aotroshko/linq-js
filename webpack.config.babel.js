5b051eae8dbaac7eefb65b9532501a735f7d8bdf
aotroshko <otroshko@gmail.com>
2016-08-28 13:05:05 +0300

    Removed unnecessary webpack plugins

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,41 +3,36 @@ import webpack from 'webpack';
 const isProduction = process.env.NODE_ENV === 'prod';
 
 let webpackPlugins = [
-  new webpack.optimize.OccurenceOrderPlugin(),
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': isProduction ? 'production' : 'development'
-  })
+	new webpack.optimize.DedupePlugin()
 ];
 
 if(isProduction) {
-    webpackPlugins.push(
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.optimize.AggressiveMergingPlugin()
-    );
+	webpackPlugins.push(
+		new webpack.optimize.UglifyJsPlugin(),
+	);
 }
 
 const config = {
-    entry: './src/index.js',
-    output: {
-        path: './dist',
-        filename: 'linq.js'
-    },
-    module: {
-        preLoaders: [
-            { test: /\.js$/, loader: 'eslint', exclude: /node_modules/ }
-        ],
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }]
-    },
-    eslint: {
-        failOnWarning: false,
-        failOnError: true
-    },
-    plugins: webpackPlugins
+	entry: './src/index.js',
+	output: {
+		path: './dist',
+		filename: 'linq.js'
+	},
+	module: {
+		preLoaders: [
+			{ test: /\.js$/, loader: 'eslint', exclude: /node_modules/ }
+		],
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader',
+		}]
+	},
+	eslint: {
+		failOnWarning: false,
+		failOnError: true
+	},
+	plugins: webpackPlugins
  };
 
- module.exports = config;
+module.exports = config;
