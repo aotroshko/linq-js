0fc63f2c51f0046c3a0a3e20e14f6147636831ef
aotroshko <otroshko@gmail.com>
2016-09-08 09:06:47 +0300

    Added Single function

---
+++ src/single.js
@@ -0,0 +1,39 @@
+import * as err from './errors.js';
+
+export default (source, predicate) => {
+	if (!source) {
+		throw new err.ArgumentNullError('source');
+	}
+
+	if (source.length === 0) {
+		throw new err.NoElementsError();
+	}
+
+	if (!predicate) {
+		if (source.length === 1) {
+			return source[0];
+		} else {
+			throw new err.MoreThanOneElementError();
+		}
+	}
+
+	let result = null;
+
+	for (let i = 0; i < source.length; i++) {
+		let element = source[i];
+
+		if (predicate(element)) {
+			if (result) {
+				throw new err.MoreThanOneMatchError();
+			} else {
+				result = element;
+			}
+		}
+	}
+
+	if (!result) {
+		throw new err.NoMatchError();
+	}
+
+	return result;
+}
