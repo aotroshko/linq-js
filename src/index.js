97b68ca0bb1e9797dc0dcac949dd75f25f0fc4fe
aotroshko <otroshko@gmail.com>
2016-09-10 14:13:34 +0300

    Added Enumerable ctor to global object

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import Enumerable from './enumerable.js';
 Array.prototype.asEnumerable = String.prototype.asEnumerable = function() {
 	return new Enumerable(this);
 };
+
+window.Enumerable = Enumerable;
